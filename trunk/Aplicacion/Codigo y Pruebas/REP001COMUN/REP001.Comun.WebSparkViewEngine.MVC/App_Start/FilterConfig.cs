45367e0ea3f77ff64b426199751ddd84a142973d
dana310300 <dana_310300@hotmail.com>
2013-05-09 10:07:08 -0500

    Actualizacion REP001 Librerias JQueryUI,Knockout

---
+++ trunk/Aplicacion/Codigo y Pruebas/REP001COMUN/REP001.Comun.WebSparkViewEngine.MVC/App_Start/FilterConfig.cs
@@ -0,0 +1,13 @@
+﻿using System.Web;
+using System.Web.Mvc;
+
+namespace REP001.Comun.WebSparkViewEngine.MVC
+{
+    public class FilterConfig
+    {
+        public static void RegisterGlobalFilters(GlobalFilterCollection filters)
+        {
+            filters.Add(new HandleErrorAttribute());
+        }
+    }
+}
---
+++ trunk/Aplicacion/Codigo y Pruebas/REP001COMUN/REP001.Comun.WebSparkViewEngine.MVC/Global.asax.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Http;
+using System.Web.Mvc;
+using System.Web.Optimization;
+using System.Web.Routing;
+using Spark.Web.Mvc;
+
+namespace REP001.Comun.WebSparkViewEngine.MVC
+{
+    // Note: For instructions on enabling IIS6 or IIS7 classic mode, 
+    // visit http://go.microsoft.com/?LinkId=9394801
+
+    public class MvcApplication : System.Web.HttpApplication
+    {
+        protected void Application_Start()
+        {
+            
+            AreaRegistration.RegisterAllAreas();
+
+            WebApiConfig.Register(GlobalConfiguration.Configuration);
+            ViewEngines.Engines.Add(new SparkViewFactory());
+            FilterConfig.RegisterGlobalFilters(GlobalFilters.Filters);
+            RouteConfig.RegisterRoutes(RouteTable.Routes);
+            BundleConfig.RegisterBundles(BundleTable.Bundles);
+        }
+    }
+}
